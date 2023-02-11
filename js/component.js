da5d8df808763887fd1c69a35c25746e88bb73ae
paulocesar89 <pc_schmitz@outlook.com>
2023-02-10 23:53:40 -0300

    inicio função javascript

---
+++ js/component.js
@@ -0,0 +1,5 @@
+const container = document.querySelector(".container")
+const thanksyou = document.querySelector(".thanks-you")
+const submitbutton = document.getElementById("envio")
+
+submitbutton.addEventListener("click", () =>{thanksyou.classList.remove("hide")})
