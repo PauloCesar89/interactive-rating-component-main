206e53770b03408fab00890463a6fb42d21c10b0
paulocesar89 <pc_schmitz@outlook.com>
2023-02-12 21:13:43 -0300

    projeto finalizado 1.0 style e funçoes javascript

--- js/component.js
+++ js/component.js
@@ -9,3 +9,13 @@ btn.addEventListener("click", function() {
     main.style.display = 'none';
     
 });
+
+var nota = document.querySelectorAll('.btn-nota');
+
+for(var i = 0; i < nota.length; i++)
+
+    nota[i].addEventListener('click', function(n) {    
+    document.getElementById("notaselecionada").innerHTML = this.textContent;
+    n.stopPropagation();
+
+});
